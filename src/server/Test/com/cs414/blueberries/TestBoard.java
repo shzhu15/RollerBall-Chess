ed6eefd4bef29b89d8ac121d3b1c08b8e52d8d6b
Jared <jaredlham@gmail.com>
2019-10-30 11:46:43 -0600

    test board initialize

--- src/server/Test/com/cs414/blueberries/TestBoard.java
+++ src/server/Test/com/cs414/blueberries/TestBoard.java
@@ -10,9 +10,29 @@ public class TestBoard {
     public void testGetPiece(){
         Board board = new Board(101);
         board.initialize();
-        Piece piece = board.getPieceAtPoint(new Point(0,2));
-        assert(piece instanceof Rook);
-        assert(piece.getPieceColor() == PieceColor.BLACK);
+        Piece piece = board.getPieceAtPoint(new Point(2,0));
+        assertTrue(piece instanceof Rook, "First Rook");
+        assertTrue(piece.getPieceColor() == PieceColor.BLACK, "First rook wrong color");
+
+        Piece piece = board.getPieceAtPoint(new Point(2,1));
+        assertTrue(piece instanceof Rook, "2nd Rook");
+        assertTrue(piece.getPieceColor() == PieceColor.BLACK, "2nd Rook wrong color");
+
+        Piece piece = board.getPieceAtPoint(new Point(3,0));
+        assertTrue(piece instanceof Bishop, "Bishop");
+        assertTrue(piece.getPieceColor() == PieceColor.BLACK, "Bishop wrong color");
+
+        Piece piece = board.getPieceAtPoint(new Point(3,1));
+        assertTrue(piece instanceof King, "King");
+        assertTrue(piece.getPieceColor() == PieceColor.BLACK, "King wrong color");
+
+        Piece piece = board.getPieceAtPoint(new Point(4,0));
+        assertTrue(piece instanceof Pawn, "Pawn 1");
+        assertTrue(piece.getPieceColor() == PieceColor.BLACK, "Pawn 1 wrong color");
+
+        Piece piece = board.getPieceAtPoint(new Point(4,1));
+        assertTrue(piece instanceof Pawn, "Pawn 2");
+        assertTrue(piece.getPieceColor() == PieceColor.BLACK, "Pawn 2 wrong color");
 
     }
 }
